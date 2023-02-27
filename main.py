7ba24abb5f2d723fd0bc1a42a6e20b2c313ab312
ThusEndingTheStory <102835906+ThusEndingTheStory@users.noreply.github.com>
2023-02-27 18:47:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Knit!!!")
+from sys import argv
+
+def err(msg):
+  print("Knit: Error: " + msg)
+  exit(1)
+
+def o():
+  try:
+    return open(argv[1], "r").readlines()
+  except:
+    err("Couldn't open target file")
+    
+print(o())
