0610f63bc94e5eaba0f79cc67f90e5ebb486f8e7
ThusEndingTheStory <102835906+ThusEndingTheStory@users.noreply.github.com>
2023-02-27 19:01:34 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
 from sys import argv
 
 def run(file):
-  for line in file:
-    words = line.split("*")
-    if words[0] == "echo":
-			print(words[1])
+	for line in file:
+		words = line.split("*")
+			if words[0] == "echo":
+				print(words[1])
 
 def err(msg):
-  print("Knit: Error: " + msg)
-  exit(1)
+	print("Knit: Error: " + msg)
+	exit(1)
 
 def o():
-  try:
-    return open(argv[1], "r").readlines()
-  except:
-    err("Couldn't open target file")
+	try:
+		return open(argv[1], "r").readlines()
+	except:
+		err("Couldn't open target file")
     
 print(o())
