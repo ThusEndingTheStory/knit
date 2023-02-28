c410684960f75243830ad9a50f2a7a156ff85c01
ThusEndingTheStory <102835906+ThusEndingTheStory@users.noreply.github.com>
2023-02-27 19:00:13 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from sys import argv
 
+def run(file):
+  for line in file:
+    words = line.split("*")
+    if words[0] == "echo":
+			print(words[1])
+
 def err(msg):
   print("Knit: Error: " + msg)
   exit(1)
